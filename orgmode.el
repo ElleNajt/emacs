f4b7dfae098cedd098e5be3dbeb3ba69bcad5ee3
Elle Najt <LNAJT4@gmail.com>
2024-02-03 15:03:36 -0500

    Hook to load in org capture after org loads

--- orgmode.el
+++ orgmode.el
@@ -1,25 +1,25 @@
 ;;; -*- lexical-binding: t; -*-
 
-
-(setq org-hierarchical-todo-statistics nil
-      +org-capture-todo-file  (concat org-directory "/inbox.org")
-      +org-capture-journal-file (concat org-directory "/journal.org")
-      org-default- (concat org-directory "/inbox.org")
-      org-capture-templates '(("a" "Todo" entry
-                               (file+headline +org-capture-todo-file  "Inbox")
-                               "* TODO %?\n%i%T" )
-                              ("t" "Todo" entry
-                               (file+headline +org-capture-todo-file  "Inbox")
-                               "* TODO %?\n%i%T" )
-                              ("l" "Linked todo" entry
-                               (file+headline +org-capture-todo-file  "Inbox")
-                               "* TODO %?\n%i\n%a%T" )
-                              ;; ("n" "Inbox-Note" entry
-                              ;;  (file+headline +org-capture-todo-file  "Inbox Note")
-                              ;; "* %?\n%i%T" )
-                              ("j" "Journal" entry
-                               (file+olp+datetree +org-capture-journal-file)
-                               "* %U %?\n%i\n%a" :prepend t))
-      org-archive-location (concat org-directory "/trash::* from %s"))
+(with-eval-after-load 'org
+  (setq org-hierarchical-todo-statistics nil
+        +org-capture-todo-file  (concat org-directory "/inbox.org")
+        +org-capture-journal-file (concat org-directory "/journal.org")
+        org-default- (concat org-directory "/inbox.org")
+        org-capture-templates '(("a" "Todo" entry
+                                 (file+headline +org-capture-todo-file  "Inbox")
+                                 "* TODO %?\n%i%T" )
+                                ("t" "Todo" entry
+                                 (file+headline +org-capture-todo-file  "Inbox")
+                                 "* TODO %?\n%i%T" )
+                                ("l" "Linked todo" entry
+                                 (file+headline +org-capture-todo-file  "Inbox")
+                                 "* TODO %?\n%i\n%a%T" )
+                                ;; ("n" "Inbox-Note" entry
+                                ;;  (file+headline +org-capture-todo-file  "Inbox Note")
+                                ;; "* %?\n%i%T" )
+                                ("j" "Journal" entry
+                                 (file+olp+datetree +org-capture-journal-file)
+                                 "* %U %?\n%i\n%a" :prepend t))
+        org-archive-location (concat org-directory "/trash::* from %s")))
 
 (setq org-log-done 't)
