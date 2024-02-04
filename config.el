3fbe5f2a1dd402b1584c73d51d2dc9ab1271a773
Elle Najt <LNAJT4@gmail.com>
2024-02-03 21:53:49 -0500

    Added black auto foramtting

--- config.el
+++ config.el
@@ -109,11 +109,10 @@
               "c" 'eval-last-sexp
               "d" 'eval-defun)))
 
-;; (use-package! python-black
-;;   :demand t
-;;   :after python)
-;; (add-hook! 'python-mode-hook #'python-black-on-save-mode)
-;; ;; Feel free to throw your own personal keybindings here
+(use-package! python-black
+  :demand t
+  :after python)
+(add-hook! 'python-mode-hook #'python-black-on-save-mode)
 ;; (map! :leader :desc "Blacken Buffer" "m b b" #'python-black-buffer)
 ;; (map! :leader :desc "Blacken Region" "m b r" #'python-black-region)
 ;; (map! :leader :desc "Blacken Statement" "m b s" #'python-black-statement)
