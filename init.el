a5d4d4d30a1cfbfd7e67aeeb075d902df99a4412
Elle Najt <LNAJT4@gmail.com>
2024-02-05 13:01:23 -0500

    added ivy fuzzy search

--- init.el
+++ init.el
@@ -25,7 +25,8 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
+       (ivy +fuzzy
+            +prescient)               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
