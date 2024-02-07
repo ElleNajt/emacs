6fd2379058a968c66d58fe105413c217c61810a3
Elle Najt <LNAJT4@gmail.com>
2024-02-06 19:53:35 -0500

    Moved Aspen tag to my personal computer org config

--- computers/Elles-MBP-after.el
+++ computers/Elles-MBP-after.el
@@ -1,7 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
 
-(add-to-list 'org-capture-templates
-             '("f" "Food journal entry" entry
-               (file+olp+datetree  "~/Documents/Notes/foodjournal.org")
-               "* %U %?\n%i" :prepend t))
+(with-eval-after-load 'org
+  (add-to-list 'org-capture-templates
+               '("f" "Food journal entry" entry
+                 (file+olp+datetree  "~/Documents/Notes/foodjournal.org")
+                 "* %U %?\n%i" :prepend t))
+
+  (add-to-list 'org-agenda-custom-commands '("pa" "Aspen" tags-todo "aspen")))
