aa7f0a31265395d0ddb0ef5785ad5d670a9c42d6
Elle Najt <lnajt4@gmail.com>
2024-09-09 17:48:09 -0400

    WIP on fixing dir-locals for based pyright

--- python/nix_project_defaults/.dir-locals.el
+++ python/nix_project_defaults/.dir-locals.el
@@ -1,5 +1,7 @@
-((python-mode
-  (python-shell-interpreter . "nix-shell --pure --run python")))
+((python-mode . (
 
-((python-mode
-  (eglot-server-programs . (((python-mode) "nix-shell" "--pure" "--run" "pyright-langserver --stdio")))))
+                 ;; (python-shell-interpreter . "nix-shell --pure --run python")
+                 ;; (eglot-server-programs . (((python-mode) "nix-shell" "--pure" "--run" "basedpyright-langserver --stdio")))
+                 (eglot-workspace-configuration . (:python (:pythonpath "nix-shell --pure --run python"
+                                                            :analysis (:extrapaths ""))))
+                 (lsp-basedpyright-extra-paths . [""]))))
