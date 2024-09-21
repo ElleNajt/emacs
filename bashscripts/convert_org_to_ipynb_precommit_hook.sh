fabf349481425b0a38d28d7ce82383984ed17317
Elle Najt <lnajt4@gmail.com>
2024-09-20 20:04:38 -0400

    add commit hook

--- bashscripts/convert_org_to_ipynb_precommit_hook.sh
+++ bashscripts/convert_org_to_ipynb_precommit_hook.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env sh
+set -x
 
-for file in $(git diff --cached --name-only --diff-filter=D | grep '.org$')
-do
+for file in $(git diff --cached --name-only --diff-filter=D | grep '.org$'); do
     base_name="${file%.org}"
     ipynb_file="${base_name}_githookgenerated.ipynb"
     rm -f "$ipynb_file"
     git rm --cached "$ipynb_file"
 done
 
-for file in $(git diff --cached --name-only --diff-filter=AM | grep '.org$')
-do
-    base_name="${file%.org}"
-    org_file="${base_name}.org"
+for org_file in $(git diff --cached --name-only --diff-filter=AM | grep '.org$'); do
+    base_name=$(basename ${org_file})
+    base_name="${base_name%.org}"
     file_directory=$(dirname "${org_file}")
-    ipynb_file="${base_name}_githookgenerated.ipynb"
-    sed -i '' "s/#+begin_src python/#+begin_src jupyter-python :exports both/gI" "$org_file" |\
-            pandoc --resource_path="$file_directory" -f org -o "$ipynb_file" -
+    ipynbs_folder_name="auto_generated_ipynbs"
+    ipynbs_dir="${file_directory}/${ipynbs_folder_name}"
+
+    [[ -d "$ipynbs_dir" ]] || mkdir "$ipynbs_dir"
+    ipynb_file="${ipynbs_dir}/${base_name}.ipynb"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/#+begin_src python/#+begin_src jupyter-python :exports both/gI" "$org_file" | pandoc --resource-path="$file_directory" -f org -o "$ipynb_file" -
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed 's/#+begin_src python/#+begin_src jupyter-python :exports both/gI' "$org_file" | pandoc --resource-path="$file_directory" -f org -o "$ipynb_file" -
+    else
+        echo "Unsupported operating system: $OSTYPE"
+        exit 1
+    fi
     git add "${ipynb_file}"
 done
