7716df51186349d7cba0bddc8e1b67624540d22a
Elle Najt <lnajt4@gmail.com>
2024-09-29 13:07:47 -0400

    Get pyright to recognize the nix-shell --
    This works because pyright uses the PYTHONPATH env var.
    So when the nix-shell sets it and is loaded by direnv, pyright picks it up.
    
    See: https://cyberchris.xyz/posts/nix-python-pyright/

--- python/nix_project_defaults/shell.nix
+++ python/nix_project_defaults/shell.nix
@@ -1,18 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
-  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-    pandas
-    numpy
-    scikit-learn
-    matplotlib
-    seaborn
-    httpx
-  ]);
-in
-pkgs.mkShell {
-  buildInputs = [
-    pythonEnv
-    pkgs.pyright
-  ];
+  pythonEnv = pkgs.python3.withPackages
+    (ps: with ps; [ pandas numpy scikit-learn matplotlib seaborn httpx ]);
+in pkgs.mkShell {
+  buildInputs = [ pythonEnv pkgs.pyright ];
+  shellHook = ''
+    PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}
+  '';
 }
+
+# Inspired by https://cyberchris.xyz/posts/nix-python-pyright/
