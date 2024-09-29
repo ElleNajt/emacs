f5119f928f05a497871dbebcc1f51e563a0ddf64
Elle Najt <lnajt4@gmail.com>
2024-09-29 17:12:31 -0400

    add dirvish bookmarks

--- computers/etude.el
+++ computers/etude.el
@@ -11,3 +11,29 @@
 
 ;;;
 ()
+
+;;; Dirvish settings
+;; taken from https://github.com/alexluigit/dirvish/blob/main/docs/CUSTOMIZING.org
+
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("D" "~/Downloads/"                "Downloads")
+     ("d" "~/.doom.d/"                "doom config")
+     ("w" "~/org/personal_webpage"        "personal_webpage")
+     ("n" "~/Notes/"                       "notes")
+     ))
+  ;; :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  ;; (setq dirvish-mode-line-format
+  ;;       '(:left (sort symlink) :right (omit yank index)))
+  ;; (setq dirvish-attributes
+  ;;       '(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
+  ;; (setq delete-by-moving-to-trash t)
+  ;; (setq dired-listing-switches
+  ;;       "-l --almost-all --human-readable --group-directories-first --no-group")
+  )
