0fab415bdbf7190baf912f85a28600d9b31f45c4
Elle Najt <lnajt4@gmail.com>
2024-09-29 15:30:03 -0400

    Removed unnecessary PYTHONPATH

--- python/nix_project_defaults/shell.nix
+++ python/nix_project_defaults/shell.nix
@@ -1,13 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  pythonEnv = pkgs.python3.withPackages
-    (ps: with ps; [ pandas numpy scikit-learn matplotlib seaborn httpx ]);
+  pythonEnv = pkgs.python3.withPackages (ps:
+    with ps; [
+      pandas
+      numpy
+      scikit-learn
+      matplotlib
+      seaborn
+      # httpx
+    ]);
 in pkgs.mkShell {
   buildInputs = [ pythonEnv pkgs.pyright ];
-  shellHook = ''
-    PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}
-  '';
 }
 
-# Inspired by https://cyberchris.xyz/posts/nix-python-pyright/
+# NOTE: To get this to be reflected in eglot, you'll need to M-x envrc-reload, then reload eglot.
