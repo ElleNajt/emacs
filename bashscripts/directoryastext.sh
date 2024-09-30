11e07301db9141c189b623037871094e5660b934
Elle Najt <lnajt4@gmail.com>
2024-09-29 20:25:59 -0400

    add envrc to dir as text

--- bashscripts/directoryastext.sh
+++ bashscripts/directoryastext.sh
@@ -2,7 +2,7 @@
 shopt -s dotglob
 shopt -s nullglob
 
-find . -type f -regex '.*\.\(py\|sh\|c\|cpp\|js\|java\|rs\|toml\|mi\|md\|txt\|json\|yaml\|yml\|nix\|env\|ini\|conf\|xml\|sql\|log\|lock\|el\)' -o -name 'Makefile' -o -name 'Dockerfile' -o -name '.gitignore' -o -name '.gitattributes' | while read -r file; do
+find . -type f -regex '.*\.\(envrc\|py\|sh\|c\|cpp\|js\|java\|rs\|toml\|mi\|md\|txt\|json\|yaml\|yml\|nix\|env\|ini\|conf\|xml\|sql\|log\|lock\|el\)' -o -name 'Makefile' -o -name 'Dockerfile' -o -name '.gitignore' -o -name '.gitattributes' | while read -r file; do
     relpath=$(realpath --relative-to="$(pwd)" "$file") # Get the relative path
     echo "===== $relpath ====="
     cat "$file"
