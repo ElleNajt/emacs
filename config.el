6b19a07ee2f94573fb554699efb9e40073aa2b2f
Elle Najt <lnajt4@gmail.com>
2025-03-21 01:01:33 -0400

    Add bionic reading

--- config.el
+++ config.el
@@ -1180,6 +1180,45 @@ it."
 ;;; oneko
 
 (use-package! oneko-macs)
+
+;;; ob-python
+
+;;; Bionic reading
+;; From http://xahlee.info/emacs/emacs/elisp_bionic_reading.html
+
+(defvar infu-bionic-reading-face nil "a face for `infu-bionic-reading-region'.")
+
+(setq infu-bionic-reading-face 'bold)
+;; try
+;; 'bold
+;; 'error
+;; 'warning
+;; 'highlight
+;; or any value of M-x list-faces-display
+
+(defun infu-bionic-reading-buffer ()
+  "Bold the first few chars of every word in current buffer.
+Version 2022-05-21"
+  (interactive)
+  (infu-bionic-reading-region (point-min) (point-max)))
+
+(defun infu-bionic-reading-region (Begin End)
+  "Bold the first few chars of every word in region.
+Version 2022-05-21"
+  (interactive "r")
+  (let (xBounds xWordBegin xWordEnd  )
+    (save-restriction
+      (narrow-to-region Begin End)
+      (goto-char (point-min))
+      (while (forward-word)
+        ;; bold the first half of the word to the left of cursor
+        (setq xBounds (bounds-of-thing-at-point 'word))
+        (setq xWordBegin (car xBounds))
+        (setq xWordEnd (cdr xBounds))
+        (setq xBoldEndPos (+ xWordBegin (1+ (/ (- xWordEnd xWordBegin) 2))))
+        (put-text-property xWordBegin xBoldEndPos
+                           'font-lock-face infu-bionic-reading-face)))))
+
 ;;; gptel
 
 
