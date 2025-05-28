d186b96ec4b0f6620fcd55cfd79b3dc2329594d7
Elle Najt <lnajt4@gmail.com>
2025-05-27 20:30:30 -0400

    mcp server

--- computers/mcp.el
+++ computers/mcp.el
@@ -10,21 +10,18 @@
 ;; ("mcp_nixos" . (:command "/home/elle/code/mcp_servers/.venv/bin/python" :args ("-m" "mcp_nixos")))
 
 (setq mcp-hub-servers
-      '(("emacs_introspection" . (:command "npx" :args ("-y" "@lnajt/emacs-introspection-mcp@latest")) )
+      '(("emacs_introspection" . (:command "npx" :args ("-y" "/home/elle/code/emacs-introspection-mcp/dist/emacs-introspection-mcp.js")) )
         ("nixos" . (:command "uvx" :args ("--install-deps" "mcp-nixos")))
         ("memory" . (:command "npx" :args ("-y" "@modelcontextprotocol/server-memory")))
         ("sequential_thinking" . (:command "npx" :args ("-y" "@modelcontextprotocol/server-sequential-thinking")))
         ("filesystem" . (:command "npx" :args ("-y" "@modelcontextprotocol/server-filesystem" "/home/elle/code/ai_agents/")))))
 
-(progn
 
-  (gptel-mcp-close-use-tool)
+(mcp-hub-restart-all-server)
 
-  (mcp-hub-restart-all-server)
-
-  (gptel-mcp-register-tool)
-
-  (gptel-mcp-use-tool)
-  )
+;; (progn
+;;   (gptel-mcp-close-use-tool)
+;;   (gptel-mcp-register-tool)
+;;   (gptel-mcp-use-tool))
 
 (setq mysecretnumber "42.00")
